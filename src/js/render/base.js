dfc4cebda0a1f59d0f92600a6a68983436ffebd8
Massimo Naccari <massimo.naccari@interlogica.it>
2018-02-21 17:32:19 +0100

    use of moment in base renderer

--- src/js/render/base.js
+++ src/js/render/base.js
@@ -1,48 +1,48 @@
-import RangeDateIterator from "../rangeDateIterator";
+import Moment from 'moment';
+import { extendMoment } from 'moment-range';
+
+const moment = extendMoment(Moment);
 
 let BaseRender = function BaseRender(season, rootElement) {
 
     this.calendar = function() {
-        var it = new RangeDateIterator(season.start, season.end);
+        var range = moment.range(season.start, season.end);
         var current_month;
         var monthElement;
 
-        while (it.valid()) {
-            var current = it.value();
+        for (let current of range.by('day')) {
             var dayElement = document.createElement('span');
 
-            if(current_month != it.value().getMonth()) {
-                current_month = it.value().getMonth();
-                monthElement = createMonthElement(it)
+            if(current_month != current.month()) {
+                current_month = current.month();
+                monthElement = createMonthElement(range)
                 rootElement.appendChild(monthElement);
             }
 
             dayElement.textContent = current.getDate();
 
-            if(typeof season.index[current.getTime()] != 'undefined') {
-                dayElement.addEventListener('click', unsplitOnClick);
-                dayElement.className = 'split';
+            if(typeof season.index[current.unix()] != 'undefined') {
+                dayElement.addEventListener('click', unsplrangeOnClick);
+                dayElement.className = 'splrange';
             } else {
-                dayElement.addEventListener('click', splitOnClick);
+                dayElement.addEventListener('click', splrangeOnClick);
             }
 
-            dayElement.setAttribute('data-date', current.getFullYear() + '_' + current.getMonth() + '_' + current.getDate());
-            dayElement.setAttribute('data-time', current.getTime());
-            dayElement.setAttribute('date-month', current.getMonth());
+            dayElement.setAttribute('data-date', current.format('YYYY-MM-DD'));
+            dayElement.setAttribute('data-time', current.unix());
+            dayElement.setAttribute('date-month', current.month());
             monthElement.appendChild(dayElement);
-
-            it.next();
         }
     };
 
 
 
-    function createMonthElement(it) {
+    function createMonthElement(range) {
         var div = document.createElement('div');
-        div.setAttribute('data-month', it.value().getMonth());
+        div.setAttribute('data-month', current.month());
         div.setAttribute('class', 'month');
         var p = document.createElement('p');
-        p.textContent = it.value().toLocaleString('it', { month: "long" });
+        p.textContent = current.toLocaleString('range', { month: "long" });
         div.appendChild(p);
 
         rootElement.appendChild(div);
@@ -50,19 +50,19 @@ let BaseRender = function BaseRender(season, rootElement) {
         return div;
     }
 
-    function splitOnClick(event) {
-        event.target.removeEventListener('click', splitOnClick);
-        event.target.addEventListener('click', unsplitOnClick);
+    function splrangeOnClick(event) {
+        event.target.removeEventListener('click', splrangeOnClick);
+        event.target.addEventListener('click', unsplrangeOnClick);
 
-        season.split(new Date(Number(this.getAttribute('data-time'))));
-        this.className = 'split';
+        season.splrange(new Date(Number(this.getAttribute('data-time'))));
+        this.className = 'splrange';
     }
 
-    function unsplitOnClick(event) {
-        this.removeEventListener('click', unsplitOnClick);
-        this.addEventListener('click', splitOnClick);
+    function unsplrangeOnClick(event) {
+        this.removeEventListener('click', unsplrangeOnClick);
+        this.addEventListener('click', splrangeOnClick);
 
-        season.unsplit(new Date(Number(this.getAttribute('data-time'))));
+        season.unsplrange(new Date(Number(this.getAttribute('data-time'))));
         this.className = '';
     }
 };
